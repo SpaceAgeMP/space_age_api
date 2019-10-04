a186968a5be11f5aaceef53970306f4c1b2f3f63
Mark Dietzer <git@doridian.net>
2019-10-04 15:05:23 -0700

    bigint

--- priv/repo/migrations/20191003052216_create_players.exs
+++ priv/repo/migrations/20191003052216_create_players.exs
@@ -5,16 +5,16 @@ defmodule SpaceAgeApi.Repo.Migrations.CreatePlayers do
     create table(:players, primary_key: false) do
       add :name, :string, null: false
       add :steamid, :string, primary_key: true
-      add :credits, :integer, null: false
-      add :score, :integer, null: false
+      add :credits, :bigint, null: false
+      add :score, :bigint, null: false
       add :is_faction_leader, :boolean, default: false, null: false
       add :is_donator, :boolean, default: false, null: false
-      add :alliance_membership_expiry, :integer, null: false
+      add :alliance_membership_expiry, :bigint, null: false
       add :faction_name, :string, null: false
       add :advancement_level, :integer, null: false
       add :research, :map, null: false
       add :station_storage, :map, null: false
-      add :playtime, :integer, null: false
+      add :playtime, :bigint, null: false
 
       timestamps()
     end
