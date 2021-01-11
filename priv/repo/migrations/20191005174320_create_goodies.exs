b8d64b1543ff1731fe465793529767212ed3a49e
Mark Dietzer <git@doridian.net>
2021-01-10 16:06:54 -0800

    Squash migrations

--- priv/repo/migrations/20191005174320_create_goodies.exs
+++ priv/repo/migrations/20191005174320_create_goodies.exs
@@ -5,10 +5,12 @@ defmodule SpaceAgeApi.Repo.Migrations.CreateGoodies do
     create table(:goodies) do
       add :steamid, :string
       add :type, :string
+      add :used, :naive_datetime
 
       timestamps()
     end
 
     create index(:goodies, [:steamid])
+    create index(:goodies, [:used])
   end
 end
