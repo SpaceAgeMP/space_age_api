a2ff61ab663bcaea4cbbec6e74b2d2544af270c2
Mark Dietzer <git@doridian.net>
2021-01-26 23:39:22 -0800

    Exclude freelancers from faction score

--- lib/space_age_api_web/controllers/factions_controller.ex
+++ lib/space_age_api_web/controllers/factions_controller.ex
@@ -10,7 +10,7 @@ defmodule SpaceAgeApiWeb.FactionsController do
         res = Repo.all(from p in Player,
                     group_by: p.faction_name,
                     select: %{faction_name: p.faction_name, credits: sum(p.credits), score: sum(p.score)},
-                    where: p.score > 0 and p.steamid != "STEAM_0:0:0",
+                    where: p.score > 0 and p.steamid != "STEAM_0:0:0" and p.faction_name != "freelancer",
                     order_by: [desc: sum(p.score)])
         render(conn, "multi_public.json", factions: res)
     end
