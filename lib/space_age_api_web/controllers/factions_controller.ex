6f07b4fc208648b9a715315210b9251a874fe27f
Mark Dietzer <git@doridian.net>
2021-01-26 23:41:35 -0800

    Why get credits if we never show it

--- lib/space_age_api_web/controllers/factions_controller.ex
+++ lib/space_age_api_web/controllers/factions_controller.ex
@@ -9,7 +9,7 @@ defmodule SpaceAgeApiWeb.FactionsController do
     def list(conn, _params) do
         res = Repo.all(from p in Player,
                     group_by: p.faction_name,
-                    select: %{faction_name: p.faction_name, credits: sum(p.credits), score: sum(p.score)},
+                    select: %{faction_name: p.faction_name, score: sum(p.score)},
                     where: p.score > 0 and p.steamid != "STEAM_0:0:0" and p.faction_name != "freelancer",
                     order_by: [desc: sum(p.score)])
         render(conn, "multi_public.json", factions: res)
--- lib/space_age_api_web/views/factions_view.ex
+++ lib/space_age_api_web/views/factions_view.ex
@@ -2,10 +2,6 @@ defmodule SpaceAgeApiWeb.FactionsView do
     @moduledoc false
     use SpaceAgeApiWeb, :view
 
-    def render("multi_public.json", %{factions: factions}) do
-        Enum.map(factions, &faction_public/1)
-    end
-
     def render("multi.json", %{factions: factions}) do
         Enum.map(factions, &faction_all/1)
     end
@@ -14,13 +10,6 @@ defmodule SpaceAgeApiWeb.FactionsView do
         %{
             faction_name: faction.faction_name,
             credits: faction.credits,
-            score: faction.score,
-        }
-    end
-
-    def faction_public(faction) do
-        %{
-            faction_name: faction.faction_name,
             score: faction.score,
         }
     end
