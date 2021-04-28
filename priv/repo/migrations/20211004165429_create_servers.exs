178b09eeedfe28ca9e94bd2e3026e2bbcade7d71
Mark Dietzer <git@doridian.net>
2021-04-28 00:10:28 -0700

    memory engine

--- priv/repo/migrations/20211004165429_create_servers.exs
+++ priv/repo/migrations/20211004165429_create_servers.exs
@@ -2,7 +2,7 @@ defmodule SpaceAgeApi.Repo.Migrations.CreateServers do
   use Ecto.Migration
 
   def change do
-    create table(:servers, primary_key: false) do
+    create table(:servers, primary_key: false, engine: "memory") do
       add :name, :string, primary_key: true
       add :map, :string, null: false, default: "sb_gooniverse_v4"
       add :players, :map, null: false
