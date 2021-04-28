ffa503437b5fc6a7c8c1d127f479de46491f984a
Mark Dietzer <git@doridian.net>
2021-04-28 00:05:43 -0700

    change keys

--- priv/repo/migrations/20210427193110_create_server_configs.exs
+++ priv/repo/migrations/20210427193110_create_server_configs.exs
@@ -2,9 +2,9 @@ defmodule SpaceAgeApi.Repo.Migrations.CreateServerConfigs do
     use Ecto.Migration
   
     def change do
-      create table(:server_configs) do
-        add :name, :string
-        add :authkey, :string, primary_key: true
+      create table(:server_configs, primary_key: false) do
+        add :name, :string, primary_key: true
+        add :authkey, :string
         add :location, :string, null: false, default: "N/A"
         add :rcon_password, :string, default: "", null: false
         add :steam_account_token, :string, default: "", null: false
@@ -12,7 +12,7 @@ defmodule SpaceAgeApi.Repo.Migrations.CreateServerConfigs do
         timestamps()
       end
   
-      create unique_index(:server_configs, [:name])
+      create unique_index(:server_configs, [:authkey])
     end
   end
   
--- priv/repo/migrations/20211004165429_create_servers.exs
+++ priv/repo/migrations/20211004165429_create_servers.exs
@@ -2,7 +2,7 @@ defmodule SpaceAgeApi.Repo.Migrations.CreateServers do
   use Ecto.Migration
 
   def change do
-    create table(:servers) do
+    create table(:servers, primary_key: false) do
       add :name, :string, primary_key: true
       add :map, :string, null: false, default: "sb_gooniverse_v4"
       add :players, :map, null: false
