6a0f92e0ef26629561e5961910b8ebce1e5646cd
Doridian <git@doridian.net>
2023-07-02 09:43:23 -0700

    squash migrations

--- priv/repo/migrations/20191003052216_create_players.exs
+++ priv/repo/migrations/20191003052216_create_players.exs
@@ -8,13 +8,15 @@ defmodule SpaceAgeApi.Repo.Migrations.CreatePlayers do
       add :credits, :bigint, null: false
       add :score, :bigint, null: false
       add :is_faction_leader, :boolean, default: false, null: false
-      add :is_donator, :boolean, default: false, null: false
-      add :alliance_membership_expiry, :bigint, null: false
       add :faction_name, :string, null: false
       add :advancement_level, :integer, null: false
       add :research, :map, null: false
       add :station_storage, :map, null: false
       add :playtime, :bigint, null: false
+      add :group, :string, default: "user", null: false
+      add :is_banned, :boolean, default: false, null: false
+      add :ban_reason, :longtext, null: true
+      add :banned_by, :string, null: true
 
       timestamps()
     end
@@ -22,5 +24,7 @@ defmodule SpaceAgeApi.Repo.Migrations.CreatePlayers do
     create index(:players, [:faction_name])
     create index(:players, [:score])
     create index(:players, [:playtime])
+    create index(:players, [:is_banned])
+    create index(:players, [:banned_by])
   end
 end
--- priv/repo/migrations/20230702133759_remove_goodies.exs
+++
@@ -1,7 +0,0 @@
-defmodule SpaceAgeApi.Repo.Migrations.RemoveGoodies do
-  use Ecto.Migration
-
-  def change do
-    drop table(:goodies)
-  end
-end
