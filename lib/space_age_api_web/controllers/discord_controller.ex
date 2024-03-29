e69c63e3f91c59366938a324f6df0947fbd30a9f
Doridian <git@doridian.net>
2024-03-28 20:40:08 -0700

    Signature to b64

--- lib/space_age_api_web/controllers/discord_controller.ex
+++ lib/space_age_api_web/controllers/discord_controller.ex
@@ -26,7 +26,7 @@ defmodule SpaceAgeApiWeb.DiscordController do
                       :eddsa,
                       :sha256,
                       "#{timestamp}#{conn.assigns[:raw_body]}",
-                      Base.decode16!(signature),
+                      Base.encode64(Base.decode16!(signature)),
                       [Application.fetch_env!(:space_age_api, :discord_public_key), :ed25519]
                   )
 
