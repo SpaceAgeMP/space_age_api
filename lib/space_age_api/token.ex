3d3d67e653cfdec6df66ec9435d9c92790e8e74f
Doridian <git@doridian.net>
2024-03-28 22:58:07 -0700

    get joken to work

--- lib/space_age_api/token.ex
+++ lib/space_age_api/token.ex
@@ -1,9 +1,14 @@
 defmodule SpaceAgeApi.Token do
     use Joken.Config
-    
+
     @default_exp 30 * 60
 
     def default_exp, do: @default_exp
 
-    def token_config, do: default_claims(default_exp: @default_exp)
+    def token_config do
+        default_claims(default_exp: @default_exp, skip: [:aud])
+        |> add_claim("aud", nil, &(&1 in [
+            "https://api.spaceage.mp/v2/clientauth"
+        ]))
+    end
 end
