03e835d24025c9fa43c71e0939b9573dbba60fc9
Doridian <git@doridian.net>
2024-03-30 01:20:32 -0700

    Fix wording, auto-delete outdated commands

--- register_discord_command.py
+++ register_discord_command.py
@@ -5,6 +5,14 @@ APP_ID = argv[1]
 BOT_TOKEN = argv[2]
 
 url = f"https://discord.com/api/v10/applications/{APP_ID}/commands"
+headers = {
+    "Authorization": f"Bot {BOT_TOKEN}"
+}
+
+old_commands = requests.get(url, headers=headers).json()
+for command in old_commands:
+    print(f"Deleting {command['name']}")
+    requests.delete(f"{url}/{command['id']}", headers=headers)
 
 # This is an example CHAT_INPUT or Slash Command, with a type of 1
 json = {
@@ -14,16 +22,14 @@ json = {
     "options": [
         {
             "name": "code",
-            "description": "Link code. Use ""unlink"" unlink any existing account",
+            "description": "Link code. Use \"unlink\" to unlink any existing account",
             "type": 3,
             "required": True
         }
     ]
 }
 
-headers = {
-    "Authorization": f"Bot {BOT_TOKEN}"
-}
+
 
 r = requests.post(url, headers=headers, json=json)
 r.raise_for_status()
