ee1de008a6e60c824b8d95f6dd32d4729ee64ada
Doridian <git@doridian.net>
2025-10-13 13:10:03 -0700

    runtime env vars

--- build.sh
+++ build.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-export DATABASE_URL="ecto://USER:PASS@HOST/DATABASE"
-export SECRET_KEY_BASE="changeme"
-export SENTRY_DSN_SRCDS=""
-export SENTRY_DSN_API=""
-
 mix local.hex --force
 mix deps.get
 mix local.rebar --force
--- flake.nix
+++ flake.nix
@@ -11,22 +11,15 @@
         src = ./.;
         version = "1.0.0";
 
-        envVars = {
-          DATABASE_URL = "ecto://space_age_api@127.0.0.1/space_age_api";
-          SECRET_KEY_BASE = "changeme";
-          SENTRY_DSN_SRCDS = "";
-          SENTRY_DSN_API = "";
-        };
-
         mixFodDeps = pkgs.beamPackages.fetchMixDeps ({
           inherit src version;
           pname = "mix-deps-${pname}";
           hash = "sha256-eTyDUQdxPWiE/RkhcPYGhXNHRVknMNxpuruHahTTXyo=";
-        } // envVars);
+        });
 
         package = pkgs.beamPackages.mixRelease ({
           inherit src pname mixFodDeps version;
-        } // envVars);
+        });
       in
       {
         packages = {
