80835d4f1ee7dbbe0fae8757ae598cf85147f512
Doridian <git@doridian.net>
2025-11-12 09:27:38 -0800

    Use MySQL socket

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,12 +1,5 @@
 import Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
-
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
     raise """
@@ -25,8 +18,11 @@ sentry_dsn_api = System.get_env("SENTRY_DSN_API") ||
     """
 
 config :space_age_api, SpaceAgeApi.Repo,
-  # ssl: true,
-  url: database_url,
+  protocol: :socket,
+  socket: System.get_env("MYSQL_SOCKET") || "/var/run/mysqld/mysqld.sock",
+  username: System.get_env("MYSQL_USERNAME") || "root",
+  password: System.get_env("MYSQL_PASSWORD") || "",
+  database: System.get_env("MYSQL_DATABASE") || "spaceage_api",
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 config :space_age_api, SpaceAgeApiWeb.Endpoint,
